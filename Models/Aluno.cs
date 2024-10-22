de23f75ffd97b56aa1ac3de2639390b01a55e0c3
Thiago Seman <thiago@sisgel.com>
2024-10-21 21:53:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Aluno.cs
@@ -0,0 +1,48 @@
+﻿namespace SenacAulaEd.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public DateTime dataCad { get; set; }
+        public string CPF { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Matricula { get; set; }
+
+        public List<Endereco> Enderecos { get; set; }
+        public List<Contato> Contatos { get; set; }
+
+    }
+
+    public enum TipoContato
+    {
+        Comercial,
+        Residencial,
+        Referencia,
+        Outros
+    }
+
+    public class Contato
+    {
+        public Guid Id { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public TipoContato Tipo { get; set; }
+        public string Site { get; set; }
+    }
+
+    public class Endereco
+    {
+        public Guid Id { get; set; }
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Pais { get; set; }
+        public string CEP { get; set; }
+    }
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,28 @@
+﻿namespace SenacAulaEd.Models
+{
+    public class Livro
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public int Ano { get; set; }
+        public int Paginas { get; set; }
+        public string Genero { get; set; }
+        public string Local { get; set; }
+        public int Qtddisponivel { get; set; }
+        public string Fornecedor { get; set; }
+        public string Edicao { get; set; }
+    }
+
+    public class Retirada
+    {
+        public Guid Id { get; set; }
+        public Livro Livro { get; set; }
+        public DateTime DataRetirada { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public DateTime DataEfetiva { get; set; }
+        public string NomeUsuario { get; set; }
+
+    }
+}
